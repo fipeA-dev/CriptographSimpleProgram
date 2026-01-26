a2684fdf960256dc1ed335bb9ecbde3ea7e04012
Felipe Amorim <trabalhofelipeamorimgarcia2007@gmail.com>
2026-01-26 02:04:38 -0300

    Add files via upload

--- Op.java
+++ Op.java
@@ -2,11 +2,14 @@ package op;
 import java.awt.Dimension;
 import java.util.Scanner;
 import op.cripto;
+import op.csar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 /**
  *
@@ -18,18 +21,34 @@ public class Op {
        JFrame janela = new JFrame();
        
        /*
-       ###test area### 
+       teste da classe csar
+       String p = " ";
+       csar.transcriptase(p);
+       */
+       
+       
+      /* ###test area### 
+       
        String p;
        String k;
        p = sc.nextLine();
        System.out.println(cripto.car(p));
-        #############
+       System.out.println(csar.transcriptase(cripto.car(p)));
        */
        
        
        
+       
+       //frame(2janela)
+       
+       JFrame janela2 = new JFrame();
+       janela2.setBounds(100,100,400,400);
+       janela2.setResizable(false);
+       
+       
+       
        //label
-        JLabel labelTitle = new JLabel("Criptograph Program 0.01v");
+        JLabel labelTitle = new JLabel("Criptograph Program 0.03v");
         labelTitle.setBounds(210, 20, 300, 30);
         janela.add(labelTitle);
         
@@ -39,50 +58,132 @@ public class Op {
         //label descripto e key
         JLabel labelMSG = new JLabel("Digite a mensagem que deseja descriptografar: ");
         JLabel labelKey = new JLabel("Digite a chave de descriptografia: ");
+        JLabel labelShift = new JLabel("Digite o shift de descriptografia: "); 
         
-        //label resp
-         JLabel labelR0 = new JLabel(" ");
-         JLabel labelR1 = new JLabel(" ");
-         JLabel labelR2 = new JLabel("A mensagem é: ");
-         JLabel labelR3 = new JLabel("A mensagem criptografada e: ");
-
+         JButton botao3 = new JButton("GO");
+         JButton botao4 = new JButton("GO");
+         
+        //text area
+        JTextArea Resp = new JTextArea();
+        Resp.setLineWrap(true);
+        
         //campo do usuario
         JTextField campoMsg = new JTextField();
         JTextField campoKey = new JTextField();    
-         campoKey.setBounds(100,160,300,30);
-         campoMsg.setBounds(100,260,300,30);
+        JTextField campoShift = new JTextField();
+        campoKey.setBounds(100,160,300,30);
+        campoMsg.setBounds(100,260,300,30);
+        campoShift.setBounds(100,360,300,30);
         //add e criar botao
-        JButton botao = new JButton("Descriptografar");
+        
+       JButton botao1 = new JButton("INICIAR");
+        botao1.setBounds(190, 80, 200, 30);
+        janela.add(botao1);
+        botao1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+        JButton botao2 = new JButton("Criptografar");
+        botao2.setBounds(310, 500, 200, 30);
+        janela.add(botao2);
+        botao2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                janela.remove(labelShift);
+                janela.remove(campoShift);
+                janela.remove(labelMSG);
+                janela.remove(campoMsg);
+                janela.remove(campoKey);
+                janela.remove(labelKey);
+                janela.remove(botao4);
+                labelMsg.setBounds(100, 100, 500, 100);
+                campoMsg.setBounds(100, 160, 300, 30);
+                janela.add(labelMsg);
+                janela.add(campoMsg);
+            
+                
+                
+                botao3.setBounds(420, 158, 90, 30);
+                janela.add(botao3);
+                botao3.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String msg = campoMsg.getText();
+                        Resp.setText(cripto.car(msg));
+                        Resp.setBounds(10, 10, 400, 400);
+                        Resp.setLineWrap(true);
+                        janela2.add(Resp);
+                        janela2.setVisible(true);
+                        campoMsg.setText("");
+                  
+                        janela.revalidate();
+                        janela.repaint();
+                        janela2.revalidate();
+                        janela2.repaint();
+            
+                 }
+            
+                });
+                
+                
+                
+                
+                  
+                janela.revalidate();
+                janela.repaint();
+            
+            }
+            
+        });
+        
+         JButton botao = new JButton("Descriptografar");
         botao.setBounds(90, 500, 200, 30);
         janela.add(botao);
         botao.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
+                  janela.remove(botao3);
                   janela.remove(campoMsg);
-                  janela.remove(labelR2);
-                  janela.remove(labelR1);
-                  janela.remove(campoKey);
-                  janela.remove(labelMSG);
-                  janela.remove(labelKey);
                   janela.remove(labelMsg);
-                  labelKey.setBounds(100, 130, 300, 30);
-                  janela.add(labelKey);
-                  labelMSG.setBounds(100, 230, 300, 30);
+                  labelMSG.setBounds(100, 100, 500, 100);
+                  labelKey.setBounds(100, 200, 500, 100);
+                  labelShift.setBounds(100, 300, 500, 100);
+                  campoKey.setBounds(100, 260, 300, 30);
+                  campoMsg.setBounds(100, 160, 300, 30);
+                  campoShift.setBounds(100,360,300,30);
+                  janela.add(labelShift);
+                  janela.add(campoShift);
                   janela.add(labelMSG);
-                  campoKey.setBounds(100,160,300,30);
-                  campoMsg.setBounds(100,260,300,30);
                   janela.add(campoMsg);
                   janela.add(campoKey);
+                  janela.add(labelKey);
                   
-                  String msg = campoMsg.getText();
-                  String key = campoKey.getText();
-                  labelR1.setText(cripto.carInverse(msg, key));
-                  labelR1.setBounds(100, 330, 300, 30);
-                  janela.add(labelR1);
-                  labelR2.setBounds(100, 300, 300, 30);
-                  janela.add(labelR2);
+                  botao4.setBounds(420, 158, 90, 30);
+                janela.add(botao4);
+                botao4.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String msg = campoMsg.getText();
+                        String key = campoKey.getText();
+                        String shift = campoShift.getText();
+                        int shift1 = Integer.parseInt(shift);
+                        
+                        Resp.setText(cripto.carInverse(msg, key, shift1));
+                        Resp.setBounds(10, 10, 400, 400);
+                        Resp.setLineWrap(true);
+                        janela2.add(Resp);
+                        janela2.setVisible(true);
+                        
+                        campoMsg.setText("");
                   
-                  
+                        janela.revalidate();
+                        janela.repaint();
+                        janela2.revalidate();
+                        janela2.repaint();
+            
+                 }
+            
+                });
                   
                   
                   
@@ -92,33 +193,10 @@ public class Op {
             }
             
         });
-       JButton botao1 = new JButton("Criptografar");
-        botao1.setBounds(310, 500, 200, 30);
-        janela.add(botao1);
-        botao1.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                janela.remove(campoMsg);
-                janela.remove(labelR1);
-                janela.remove(labelR2);
-                janela.remove(labelMSG);
-                janela.remove(labelKey);
-                janela.remove(labelMsg);
-                labelMsg.setBounds(100, 130, 300, 30);
-                janela.add(labelMsg);
-                campoMsg.setBounds(100,160,300,30);
-                janela.add(campoMsg);
-                janela.remove(campoKey);
-                
-                
-                String msg = campoMsg.getText();
-                labelR0.setText(cripto.car(msg));
-                labelR3.setBounds(100, 190, 300, 30);
-                janela.add(labelR3);
-                labelR0.setBounds(100, 210, 300, 30);
-                labelR0.setPreferredSize(new Dimension(300, 500));
-                janela.add(labelR0);
-                
+        
+        
+        
+        
                 
                 janela.revalidate();
                 janela.repaint();
@@ -145,6 +223,7 @@ public class Op {
        //janela
         janela.setLayout(null);
         janela.setBounds(350, 55, 600, 600);
+        janela.setResizable(false);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setVisible(true);
     }
--- cripto.java
+++ cripto.java
@@ -1,58 +1,119 @@
 package op;
+
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.math.BigInteger;
+
 /**
- *
- * @author fipeA-dev
+ * Criptografia XOR + HEX + Cifra de César para hexadecimal
+ * Funciona com qualquer string, retorna HEX + key + shift
  */
 public class cripto {
-      
-    public static String car(String msg){
+
+    private static final char[] HEX = {
+        '0','1','2','3','4','5','6','7',
+        '8','9','A','B','C','D','E','F'
+    };
+
+    // símbolos temporários (não existem no hexadecimal)
+    private static final char[] TMP = {
+        'g','h','i','j','k','l','m','n',
+        'o','p','q','r','s','t','u','v'
+    };
+
+    /**
+     * Criptografa uma mensagem
+     * @param msg texto puro
+     * @return String formatada: HEX + \n chave Base64 + \n shift
+     */
+    public static String car(String msg) {
         byte[] dados = msg.getBytes(StandardCharsets.UTF_8);
         SecureRandom random = new SecureRandom();
+
+        // XOR
         byte[] key = new byte[dados.length];
         random.nextBytes(key);
         byte[] crip = new byte[dados.length];
-        
-        for(int i = 0; i <dados.length; i++){
-            crip[i]= (byte) (dados[i]^key[i]);
+        for(int i = 0; i < dados.length; i++){
+            crip[i] = (byte) (dados[i] ^ key[i]);
         }
+
+        // Binário → HEX
         StringBuilder bin = new StringBuilder();
         for(byte b : crip){
-            bin.append(String.format("%8s", 
-                    Integer.toBinaryString(b & 0xFF)).replace(' ','0'));
+            bin.append(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ','0'));
         }
-        String keyBase64 = Base64.getEncoder().encodeToString(key);
-        return("<html>"+bin.toString()+" "+keyBase64+"<html>");
-        //*tring keyBase64 = Base64.getEncoder().encodeToString(key);
-        /*System.out.println("A chave de descriptografia e: "+keyBase64);
-        
-        */
-    }
-       
-            
-     public static String carInverse(String bin, String keyB64){
-            byte[] key = Base64.getDecoder().decode(keyB64);
-            int n = bin.length()/8;
-            byte[] descrip = new byte[n];
-            
-            for(int i=0; i<n; i++){
-                String bloco = bin.substring(i*8, i*8+8);
-                descrip[i] = (byte) Integer.parseInt(bloco, 2);
-                
+        BigInteger decimal = new BigInteger(bin.toString(), 2);
+        String hex = decimal.toString(16).toUpperCase();
+
+        // Adiciona zero à esquerda se necessário para múltiplo de 2
+        if(hex.length() % 2 != 0) hex = "0" + hex;
+
+        // Cifra de César HEX
+        int shift = random.nextInt(16);
+        char[] chars = hex.toCharArray();
+        for(int i = 0; i < chars.length; i++){
+            int idx = indexOf(HEX, chars[i]);
+            if(idx != -1){
+                chars[i] = HEX[(idx + shift) % 16];
             }
-            byte[] original = new byte[n];
-            for(int i =0;i<n;i++){
-                original[i] = (byte) (descrip[i]^key[i]);
-            }        
-            String result = new String(original, StandardCharsets.UTF_8);
-            return result;
-     }
-            
-            
-        
-        
+        }
+
+        // chave Base64
+        String keyBase64 = Base64.getEncoder().encodeToString(key);
+
+        return new String(chars) + "\n" + keyBase64 + "\n" + shift;
+    }
+
+    /**
+     * Descriptografa a mensagem
+     * @param hexShift HEX cifrado com César
+     * @param keyB64 chave Base64
+     * @param shift shift usado na cifra de César
+     * @return String original
+     */
+    public static String carInverse(String hexShift, String keyB64, int shift) {
+        // Desfaz César
+        char[] chars = hexShift.toCharArray();
+        for(int i = 0; i < chars.length; i++){
+            int idx = indexOf(HEX, chars[i]);
+            if(idx != -1){
+                chars[i] = HEX[(idx - shift + 16) % 16];
+            }
+        }
+        String hex = new String(chars);
+
+        // HEX → binário
+        BigInteger decimal = new BigInteger(hex, 16);
+        String bin = decimal.toString(2);
+
+        // padding correto
+        int totalBytes = Base64.getDecoder().decode(keyB64).length;
+        int totalBits = totalBytes * 8;
+        bin = String.format("%" + totalBits + "s", bin).replace(' ', '0');
+
+        // binário → bytes
+        byte[] descrip = new byte[totalBytes];
+        for(int i = 0; i < totalBytes; i++){
+            descrip[i] = (byte) Integer.parseInt(bin.substring(i*8, i*8+8), 2);
+        }
+
+        // XOR com a chave
+        byte[] key = Base64.getDecoder().decode(keyB64);
+        byte[] original = new byte[totalBytes];
+        for(int i = 0; i < totalBytes; i++){
+            original[i] = (byte) (descrip[i] ^ key[i]);
+        }
+
+        return new String(original, StandardCharsets.UTF_8);
+    }
+
+    // Função auxiliar para encontrar índice em array
+    private static int indexOf(char[] arr, char c){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == c) return i;
+        }
+        return -1;
+    }
 }
-
-
